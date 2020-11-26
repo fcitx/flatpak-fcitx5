f1a29d72961515235d4fccb993c1aa01fd9e36c4
Weng Xuetian <wengxt@gmail.com>
2020-11-25 19:54:18 -0800

    Add initial impl for flatpak

---
+++ fcitx5.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+export FCITX_ADDON_DIRS=/app/lib/fcitx5
+for dir in `ls /app/addons/`; do
+    export FCITX_ADDON_DIRS=/app/addons/$dir/lib/fcitx5:$FCITX_ADDON_DIRS
+    export XDG_DATA_DIRS=/app/addons/$dir/share:${XDG_DATA_DIRS}
+done
+
+exec fcitx5-bin "$@"
