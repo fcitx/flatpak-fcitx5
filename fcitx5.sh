3011534b2aa7812da085a8905b33598027083bce
Weng Xuetian <wengxt@gmail.com>
2020-11-28 21:08:59 -0800

    Remove unneeded files and fix PATH

--- fcitx5.sh
+++ fcitx5.sh
@@ -4,6 +4,7 @@ export FCITX_ADDON_DIRS=/app/lib/fcitx5
 for dir in `ls /app/addons/`; do
     export FCITX_ADDON_DIRS=/app/addons/$dir/lib/fcitx5:$FCITX_ADDON_DIRS
     export XDG_DATA_DIRS=/app/addons/$dir/share:${XDG_DATA_DIRS}
+    export PATH=/app/addons/$dir/bin:$PATH
 done
 
 exec fcitx5-bin "$@"
