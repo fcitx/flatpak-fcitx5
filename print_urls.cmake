541879983e493135bda0bdd6b4cacf6e3d6cffb4
Weng Xuetian <wengxt@gmail.com>
2026-07-28 21:36:11 -0700

    Add libime data update workflow

---
+++ print_urls.cmake
@@ -0,0 +1,19 @@
+cmake_minimum_required(VERSION 3.16)
+
+function(fcitx5_download name url file sha256)
+  message("${url} ${sha256}")
+endfunction()
+
+function(fcitx5_extract)
+endfunction()
+function(add_custom_command)
+endfunction()
+function(add_custom_target)
+endfunction()
+function(install)
+endfunction()
+
+set(LIBIME_INSTALL_LIBDATADIR "")
+set(LIBIME_INSTALL_PKGDATADIR "")
+
+include("${INPUT_FILE}")
